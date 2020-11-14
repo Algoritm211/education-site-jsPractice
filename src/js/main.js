36640aba699dc016b97b2aedcf6a3670d48edf6a
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-11-14 12:27:38 +0200

    Add PlayYouTubeVideo module (IMPORTANT FOR FUTURE PROJECTS)

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
+import VideoPlayer from './modules/playVideo'
 import Slider from './modules/slider'
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next')
   slider.render()
+  const video = new VideoPlayer('.showup .play', '.overlay')
+  video.init()
 })
---
+++ src/js/modules/playVideo.js
@@ -0,0 +1,56 @@
+class VideoPlayer {
+  constructor (triggers, overlay) {
+    this.buttons = document.querySelectorAll(triggers)
+    this.overlay = document.querySelector(overlay)
+    this.close = this.overlay.querySelector('.close')
+  }
+
+  bindTriggers() {
+    this.buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Если плеер уже существует, то просто открываем окно без создания плеера
+        // Если плеера еще нет, то вызываем createPlayer и создаем новый плеер вместо <div id="frame"></div>
+        if (document.querySelector('iframe#frame')) {
+          this.overlay.style.display = 'flex'
+        } else {
+          const path = button.getAttribute('data-url')
+          this.createPlayer(path)
+        }
+      })
+    })
+  }
+
+  bindCloseBtn() {
+    this.close.addEventListener('click',() => {
+      this.overlay.style.display = 'none'
+      this.player.stopVideo()
+    })
+  }
+
+  createPlayer(url) {
+    this.player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: `${url}`
+    });
+
+    console.log(this.player);
+    this.overlay.style.display = 'flex'
+  }
+
+
+  init() {
+    const tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    this.bindTriggers()
+    this.bindCloseBtn()
+  }
+
+
+}
+
+
+export default VideoPlayer
